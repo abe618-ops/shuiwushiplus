d1e88051918dd2886b4d5a84623d45c7e31b89b3
abe618-ops <abe618@gmail.com>
2026-09-08 12:51:24 +0800

    Bump OddsFlow to 0.2.0

--- projects/odds-flow-android/app/build.gradle.kts
+++ projects/odds-flow-android/app/build.gradle.kts
@@ -7,8 +7,8 @@ android {
         applicationId = "com.abe618.oddsflow"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
     }
     buildTypes { release { isMinifyEnabled = false } }
 }
