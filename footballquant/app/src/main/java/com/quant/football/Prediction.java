7bec1d512119fb69d8b12b8fa06749ee5179dff3
abe618-ops <abe618@gmail.com>
2026-09-08 12:40:44 +0800

    Add score-matrix odd-even probability field

--- footballquant/app/src/main/java/com/quant/football/Prediction.java
+++ footballquant/app/src/main/java/com/quant/football/Prediction.java
@@ -4,7 +4,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class Prediction {
-    public double home, draw, away, over25;
+    public double home, draw, away, over25, oddGoalsProb;
     public double lambdaHome,lambdaAway,expectedGoals,bttsYes,homeZero,awayZero,anyZero;
     public double favWin1,favWin2,favWin3Plus;
     public String favoriteSide="",distribution="",grade="C",primary="",secondary="",totalPick="",comment="";
@@ -14,7 +14,7 @@ public class Prediction {
     public int bookmakerCount=0;
 
     public JSONObject toJson(MatchInfo m){JSONObject o=new JSONObject();try{
-        o.put("match",m.toJson());o.put("home",home);o.put("draw",draw);o.put("away",away);o.put("over25",over25);
+        o.put("match",m.toJson());o.put("home",home);o.put("draw",draw);o.put("away",away);o.put("over25",over25);o.put("oddGoalsProb",oddGoalsProb);
         o.put("lambdaHome",lambdaHome);o.put("lambdaAway",lambdaAway);o.put("expectedGoals",expectedGoals);o.put("bttsYes",bttsYes);
         o.put("homeZero",homeZero);o.put("awayZero",awayZero);o.put("anyZero",anyZero);o.put("favWin1",favWin1);o.put("favWin2",favWin2);o.put("favWin3Plus",favWin3Plus);o.put("favoriteSide",favoriteSide);
         o.put("distribution",distribution);o.put("grade",grade);o.put("primary",primary);o.put("secondary",secondary);o.put("totalPick",totalPick);o.put("comment",comment);
